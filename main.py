e77c1749cad27903802f2c69c7102e2e7416d83a
Brice Vadnais <bricevadnais@yahoo.com>
2022-11-08 10:59:21 -0500

    check if other users have posted videos and if so add them as well

--- main.py
+++ main.py
@@ -23,16 +23,45 @@ class Bot:
         time.sleep(2)
 
     def post_video_link_to_subreddit(
-        self, video_title=None, video_link=None, subreddit=None
+        self,
+        video_title=None,
+        video_link=None,
+        subreddit=None,
     ):
-        if not video_title or video_title is None:
+        if not video_title or video_title is None or video_title == "":
             raise Exception("post_video_link_to_subreddit: video_title is None")
-        if not video_link or video_link is None:
+        if not video_link or video_link is None or video_link == "":
             raise Exception("post_video_link_to_subreddit: video_link is None")
-        if not subreddit or subreddit is None:
+        if not subreddit or subreddit is None or subreddit == "":
             raise Exception("post_video_link_to_subreddit: subreddit is None")
         time.sleep(2)
         self.reddit.subreddit(subreddit).submit(video_title, url=video_link)
+
+    def add_posted_links_by_others(self, subreddit, db: cache.DB):
+        if not subreddit or subreddit is None or subreddit == "":
+            raise Exception("add_posted_links_by_others: subreddit is None")
+        if not db or db is None:
+            raise Exception("add_posted_links_by_others: db is None")
+        time.sleep(2)
+        for submission in self.reddit.subreddit(subreddit).new(limit=120):
+            if submission.author == self.reddit.user.me():
+                continue
+            print(f"found submission from to add to db: {submission_to_s(submission)}")
+            url = submission.url
+            if "youtu.be" in url:
+                split_link = url.split(".be/")[1].split("&")[0]
+                url_to_add = f"https://www.youtube.com/watch?v={split_link}"
+                db.add_to_posted_on_reddit(url_to_add)
+                print(f"added url: {url_to_add}")
+            if "youtube.com/watch" in url:
+                split_link = url.split("/watch?v=")[1].split("&")[0]
+                url_to_add = f"https://www.youtube.com/watch?v={split_link}"
+                db.add_to_posted_on_reddit(url_to_add)
+                print(f"added url: {url_to_add}")
+
+
+def submission_to_s(submission) -> str:
+    return f"Submission(id={submission.id}, author={submission.author}, title=`{submission.title}`, url={submission.url})"
 
 
 def start_bot_process(channel_ids_fname=None, subreddit_to_post_to=None):
@@ -49,6 +78,8 @@ def start_bot_process(channel_ids_fname=None, subreddit_to_post_to=None):
     bot.login()
     channel_ids = None
     while True:
+        # First make sure we add all other videos that have not been posted by the bot
+        bot.add_posted_links_by_others(subreddit=subreddit_to_post_to, db=cache_db)
         with open(channel_ids_fname) as f:
             channel_ids = f.readlines()
             channel_ids = [x.replace("\r", "").replace("\n", "") for x in channel_ids]
